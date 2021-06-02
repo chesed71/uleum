c05ddc0ba8023c6b4bfdd2ddbe2c63170aa13a3f
chesed71 <chesed71@gmail.com>
2021-06-02 17:35:59 +0900

    첫 프로젝트 파일들

---
+++ app.py
@@ -0,0 +1,24 @@
+import sys
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/user")
+def show_user():
+    print('enter getJSONReuslt', flush=True)
+    return """<!DOCTYPE html>
+<html>
+<head>
+<title>타이틀</title>
+</head>
+<body>
+<h1>저의 첫 웹사이트 오신것을 환영합니다.</h1>
+</body>
+</html>
+"""
+
+if __name__ == "__main__":
+    app.run(debug=True, port=26900)
